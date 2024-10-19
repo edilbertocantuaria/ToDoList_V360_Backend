632fafca11aa24ccbcb2b0ce0837f36bc3c93504
edilbertocantuaria <edilbertounbfga@gmail.com>
2024-10-19 01:02:01 -0300

    FEAT: bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
-end
+    has_secure_password
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :name, presence: true
+    validates :password, presence: true, length: { minimum: 6 }
+  end
